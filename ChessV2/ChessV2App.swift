df3e49ff94e3d28d86c69c193a0e25efe8ad9e0c
AnTran <cao.an99@gmail.com>
2024-07-03 17:53:42 +0700

    Initial Commit

---
+++ ChessV2/ChessV2App.swift
@@ -0,0 +1,17 @@
+//
+//  ChessV2App.swift
+//  ChessV2
+//
+//  Created by Trần Ân on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ChessV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChessV2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChessV2
+//
+//  Created by Trần Ân on 3/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
